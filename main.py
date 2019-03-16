371c708a9793dcf9c363a63c2b7a22ef0ce200e3
Nick Krumholz <nick.krumholz@gmail.com>
2019-03-16 11:05:23 -0700

    Replaced flask-sslify with flask-talisman

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from flask import Flask, render_template
-from flask_sslify import SSLify
+from flask_talisman import Talisman
 
 app = Flask(__name__)
-sslify = SSLify(app, permanent=True)
-
+talisman = Talisman(
+    app,
+    content_security_policy={
+        'default-src': '\'self\'',
+        'img-src': [
+            '\'self\'',
+            '*.google-analytics.com',
+            '*.doubleclick.net',
+            '*.google.com'
+        ],
+        'script-src': [
+            '\'self\'',
+            '*.googletagmanager.com'
+        ],
+        'style-src': [
+            '\'self\'',
+            '*.googleapis.com'
+        ],
+        'font-src': [
+            '\'self\'',
+            '*.googleapis.com',
+            '*.gstatic.com'
+        ]
+    },
+    content_security_policy_nonce_in=['script-src']
+)
 
 @app.route("/")
 @app.route("/home")
@@ -12,4 +36,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
